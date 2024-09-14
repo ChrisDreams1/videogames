b3688713597ccd1e02710e0fd0246d57c8e0ad13
Christian <cristianandresgar13@gmail.com>
2024-09-13 21:33:12 -0500

    Number race inicio

---
+++ Scrips Python/number_race/main.py
@@ -0,0 +1,14 @@
+"""
+scrip description: Numer race
+Dev: Christian G.
+Date: 13/09/2024
+"""
+
+from random import randint
+
+
+dice1 = randint(1, 6)
+dice2 = randint(1, 6)
+
+print (f"Dice1: {dice1}")
+print (f"Dice1: {dice2}")
